cabe6173c22cf99ad153a52ff725d0cd638ceb51
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2026-06-25 12:29:24 +1200

    Add automerge tarstate writer

--- packages/tarstate-automerge/src/index.ts
+++ packages/tarstate-automerge/src/index.ts
@@ -2,3 +2,8 @@ export { useDocument, useDocumentMap } from './document.js'
 export type { AutomergeSourceOptions } from './source.js'
 export { useAutomergeSource } from './source.js'
 export { useAutomergeQueries } from './query.js'
+export type { AutomergeObjectDoc } from './write.js'
+export {
+  applyAutomergeTransaction,
+  createAutomergeWriter,
+} from './write.js'
